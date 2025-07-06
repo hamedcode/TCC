dd845e945148d102a520ae2a6bf6be8f9a4d66d6
hamed1124 <92687021+hamed1124@users.noreply.github.com>
2025-07-06 21:32:18 +0330

    Update test_acha.py

--- test_acha.py
+++ test_acha.py
@@ -1,4 +1,19 @@
+import os
+import base64
+from pyrogram import Client
+
+# اطلاعات احراز هویت
+SESSION_NAME = "pyrogram_config_collector"
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+SESSION_B64 = os.getenv("PYROGRAM_SESSION_B64")
+
+# ساخت فایل session از secret
+with open(f"{SESSION_NAME}.session", "wb") as f:
+    f.write(base64.b64decode(SESSION_B64))
+
+# اجرای کلاینت Pyrogram و گرفتن پیام
 with Client(SESSION_NAME, api_id=API_ID, api_hash=API_HASH) as app:
-    msg = app.get_messages("@Achavpn", 10699)
-    content = msg.text or msg.caption
-    print("📩 محتوای پیام 10699:\n", content)
+    message = app.get_messages("@Achavpn", 10699)
+    content = message.text or message.caption or "(هیچی نیست)"
+    print(f"📨 پیام 10699 از @Achavpn:\n{content}")
